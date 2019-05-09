839aa6f43a8f09a02cdd1fda3d1c5754941c11e1
takeplace.co <gitlab.3d3013@tryninja.io>
2019-05-09 18:13:23 +0200

    UnicodeError added, New function get_title

--- bot.py
+++ bot.py
@@ -12,25 +12,53 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 admin = [135605474, 311495487] # 311495487 
 channel_id = -1001261875848
 FIFO = []
+PRODUCTION = False
 
 logging.basicConfig(
         filename='techstuff.log',
         filemode='a',
         level=logging.INFO,
         format='%(asctime)s [%(levelname)s] %(name)s:  %(message)s')
-       
-
 l = logging.getLogger(__name__)
+
 try:
     with open('fifo.json') as ff:
         l.info("Opening Queue file")
         FIFO = json.load(ff)
         l.info("Queue loaded correctly")
-
 except:
     l.error("Queue file not found, creating a new file")
     with open('fifo.json', 'w') as ff:
             json.dump(FIFO, ff)
+
+
+def get_title(link):
+    text = None
+    try:
+        r = requests.get(link)
+        title = fromstring(r.content).findtext('.//title')
+        text = "*{}* \n {}".format(title, link) 
+    
+    except requests.exceptions.InvalidSchema:
+        up.message.reply_text("Link errato o non schema invalido!")
+        l.error("Link not valid, Invalid Schema, link: {}".format(link), exec_inf=True)
+
+    except SocketError as e:
+        send_admin(bot, str(e))
+        l.error("SocketError:" , exec_inf=True)
+        
+    except UnicodeEncodeError as e: 
+        l.error("Unicode Error: re-encoding in utf-8")
+        title = title.encode('utf-8')
+        if text in FIFO:
+            l.warning("Link gia presente nella coda FIFO")
+            return False
+
+    except Exception as e:
+        up.message.reply_text(str(e))
+        l.error("General error, link: {}".format(link), exec_inf=True)
+
+    return text
 
 
 def start(bot, update):
@@ -61,14 +89,11 @@ def send_link(bot, job):
 
 
 def save_link(bot, up):
+    title = None
     try:
         link = up.message.text
         l.info("New message incoming, handled as link {}".format(link))
-        r = requests.get(link)
-        tree = fromstring(r.content)
-        title = tree.findtext('.//title')
-        text = "*{}* \n {}".format(title, link)
-
+        text = get_title(link)
         if text in FIFO:
             l.warning("Link gia presente nella coda FIFO")
             up.message.reply_text("Questo link esiste gia nella FIFO!")
@@ -86,6 +111,15 @@ def save_link(bot, up):
         send_admin(bot, str(e))
         l.error("SocketError:" , exec_inf=True)
         
+    except UnicodeEncodeError as e: 
+        l.error("Unicode Error: re-encoding in utf-8")
+        title = title.encode('utf-8')
+        if text in FIFO:
+            l.warning("Link gia presente nella coda FIFO")
+            up.message.reply_text("Questo link esiste gia nella FIFO!")
+            return False
+        FIFO.append(title)
+
     except Exception as e:
         up.message.reply_text(str(e))
         l.error("General error, link: {}".format(link), exec_inf=True)
@@ -95,7 +129,6 @@ def save_link(bot, up):
         with open('fifo.json', 'w') as ff:
             json.dump(FIFO, ff)
 
-    return True 
 
 
 def queue(b,u):
@@ -131,18 +164,14 @@ def send_admin(bot, message):
 
 
 def insert(bot, message):
-    link = message.message.text.split(' ')
-    index = link[1] 
-    link = link[2]
+    data = message.message.text.split(' ')
+    index = data[1] 
+    link = data[2]
     
     try:
         l.info("Insert incoming, handled as link {}".format(link))
-        r = requests.get(link)
-        tree = fromstring(r.content)
-        title = tree.findtext('.//title')
-        text = "*{}* \n {}".format(title, link)
-        l.info("Text formatted: {}".format(title) )
-
+        text = get_title(link)
+        l.info("Text formatted: {}".format(text) )
         if text in FIFO:
             l.warning("Link gia presente nella coda FIFO")
             up.message.reply_text("Questo link esiste gia nella FIFO!")
@@ -151,14 +180,6 @@ def insert(bot, message):
         l.info("Appending link on Queue")
         FIFO.append(text)
         up.message.reply_text("New element appended on queue, {} in list".format(len(FIFO)))
-
-    except requests.exceptions.InvalidSchema:
-        up.message.reply_text("Link errato o non schema invalido!")
-        l.error("Link not valid, Invalid Schema, link: {}".format(link), exec_inf=True)
-
-    except SocketError as e:
-        send_admin(bot, str(e))
-        l.error("SocketError:" , exec_inf=True)
         
     except Exception as e:
         up.message.reply_text(str(e))
@@ -191,10 +212,12 @@ def main():
 
     dp.add_handler(MessageHandler(Filters.text, save_link))
     
-    # week days posting times 
     
-    # updater.job_queue.run_daily(send_link, datetime.datetime.today())
-
+    # UNCOMMENT WHEN DEBUGGING
+    #updater.job_queue.run_daily(send_link, datetime.datetime.today())
+    #channel_id = -1001480479440
+    
+    # week days posting times 
     updater.job_queue.run_daily(send_link, datetime.time(8, 00), days=(0,1,2,3,4))
     updater.job_queue.run_daily(send_link, datetime.time(9, 00), days=(0,1,2,3,4))
     updater.job_queue.run_daily(send_link, datetime.time(12, 30), days=(0,1,2,3,4))
